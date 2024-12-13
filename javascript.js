e2aaee54ff17dea0209370a8b306d5f68129091a
RONPOV <172224934+RonPov@users.noreply.github.com>
2024-12-12 23:58:32 -0500

    create the getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
 function getComputerChoice() {
-    
+    const choice = Math.random();
+
+    if (choice >= 0 && choice <= 0.33)
+    {
+        return 'Rock'
+    }
+    else if (choice > 0.33 && choice <= 0.66)
+    {
+        return 'Paper'
+    }
+    else
+    {
+        return 'Scissors'
+    }
 }
